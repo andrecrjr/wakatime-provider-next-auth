0e1a7863cb3d508c6dd815f4721ec42933df41e4
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-03-19 22:55:17 -0300

    fix: removing axios

--- src/Wakatime.ts
+++ src/Wakatime.ts
@@ -1,51 +1,57 @@
 import { OAuthConfig, OAuthUserConfig } from "next-auth/providers"
 import { tokenConverter } from "./utils";
-import { RootUserWakatimeProfile } from './types'
-import axios from "axios";
+import { RootUserWakatimeProfile, UserWakatimeProfile } from './types'
 
 export default function WakatimeProvider<G extends RootUserWakatimeProfile>( options:OAuthUserConfig<G>):OAuthConfig<G>{
-    return {
+    return  {
       id: "wakatime",
       name: "Wakatime",
       type: "oauth",
+      clientId: process.env.WAKATIME_CLIENT_ID,
+      clientSecret: process.env.WAKATIME_CLIENT_SECRET,
       version: "2.0",
       token: {
         url:"https://wakatime.com/oauth/token",
-          async request(context:any) {
-            // todo: type for context
-              const response = await axios.post("https://wakatime.com/oauth/token",
-              {...context.params, redirect_uri:context.provider.callbackUrl},
-            );
-            const tokensWakatime = tokenConverter(response.data)
-            return {tokens:tokensWakatime}
-          }, 
-          params:{
-            client_id:options.clientId,
-            client_secret:options.clientSecret,
-            grant_type:"authorization_code"
-          },
-        },
+        async request(context) {
+          const response = await fetch("https://wakatime.com/oauth/token", {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({ ...context.params, 
+              redirect_uri: context.provider.callbackUrl })
+          });
+          const data = await response.text()
+          const tokenWaka = tokenConverter(data)
+          return {tokens:tokenWaka}
+        }, 
+        params:{
+          client_id:process.env.WAKATIME_CLIENT_ID,
+          client_secret:process.env.WAKATIME_CLIENT_SECRET,
+          grant_type:"authorization_code"
+        }
+      },
       authorization: {
-          url:"https://wakatime.com/oauth/authorize?response_type=code",
-          params: {
+          url:"https://wakatime.com/oauth/authorize?response_type=code", params:
+        {
           scope:"email,read_stats,read_summaries",
           "grant_type":"authorization_code"
         }
       },
-      userinfo: {
-        async request(context:any){
-          // todo: type for context
-          const response = await axios.get("https://wakatime.com/api/v1/users/current", {
-            headers: {Authorization: `Bearer ${context.tokens.access_token}`}
-          })
-          return response.data
-        },
-      },
-      profile(profile:RootUserWakatimeProfile) {
-        const data = profile.data
+    userinfo: {
+      async request(context){
+        const response = await fetch("https://wakatime.com/api/v1/users/current", {
+          headers: {Authorization: `Bearer ${context.tokens.access_token}`}
+        })
+        const data = await response.json()
         return data
-      },
-      accessTokenUrl: "https://wakatime.com/oauth/token",
-      options
       }
+    },
+    idToken:false,
+    profile(profile) {
+      const data: UserWakatimeProfile = profile.data
+      return {id:data.id, name:data.username, email:data.email, image:data.photo}
+    },
+    accessTokenUrl: "https://wakatime.com/oauth/token",
+    }
 }
