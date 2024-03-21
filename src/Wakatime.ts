332e8db397378009ae72f4c3ac65b6a1a80d4a23
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2024-03-21 00:40:27 -0300

    - formatting code

--- src/Wakatime.ts
+++ src/Wakatime.ts
@@ -3,8 +3,6 @@ import { tokenConverter } from "./utils";
 import { RootUserWakatimeProfile, UserWakatimeProfile } from './types'
 
 export default function WakatimeProvider<G extends RootUserWakatimeProfile>( options:OAuthUserConfig<G>):OAuthConfig<G>{
-
-
     return  {
       id: "wakatime",
       name: "Wakatime",
@@ -15,22 +13,22 @@ export default function WakatimeProvider<G extends RootUserWakatimeProfile>( opt
       token: {
         url:"https://wakatime.com/oauth/token",
         async request(context) {
-          const response = await fetch("https://wakatime.com/oauth/token", {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({ ...context.params, 
-              redirect_uri: context.provider.callbackUrl })
-          });
-          const data = await response.text()
-          const tokenWaka = tokenConverter(data)
-          return {tokens:tokenWaka}
+            const response = await fetch("https://wakatime.com/oauth/token", {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              body: new URLSearchParams({ ...context.params, 
+                redirect_uri: context.provider.callbackUrl })
+            });
+            const data = await response.text()
+            const tokenWaka = tokenConverter(data)
+            return {tokens:tokenWaka}
         }, 
         params:{
-          client_id:options.clientId,
-          client_secret:options.clientSecret,
-          grant_type:"authorization_code"
+            client_id:options.clientId,
+            client_secret:options.clientSecret,
+            grant_type:"authorization_code"
         }
       },
       authorization: {
@@ -38,15 +36,15 @@ export default function WakatimeProvider<G extends RootUserWakatimeProfile>( opt
           params:{
           scope:`email,read_stats`,
           "grant_type":"authorization_code"
-        }
+          }
       },
     userinfo: {
       async request(context){
-        const response = await fetch("https://wakatime.com/api/v1/users/current", {
-          headers: {Authorization: `Bearer ${context.tokens.access_token}`}
-        })
-        const data = await response.json()
-        return data
+          const response = await fetch("https://wakatime.com/api/v1/users/current", {
+            headers: {Authorization: `Bearer ${context.tokens.access_token}`}
+          })
+          const data = await response.json()
+          return data
       }
     },
     idToken:false,
